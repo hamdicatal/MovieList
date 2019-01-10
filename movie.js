75f1d1542daee262702b015ebd81656406d38358
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-01-10 22:57:28 +0300

    Storage ve UI işlemleri ES6 uygun şekilde düzenlendi

--- movie.js
+++ movie.js
@@ -1,5 +1,7 @@
-function Movie(title, director, url){
-    this.title = title;
-    this.director = director;
-    this.url = url;
+class Movie{
+    constructor(title, director, url){
+        this.title = title;
+        this.director = director;
+        this.url = url;
+    }
 }
--- storage.js
+++ storage.js
@@ -1,39 +1,37 @@
-function Storage(){
-
-}
-
-// add new movie to local storage
-Storage.prototype.addMovieToStorage = function(newMovie){
-    let movies = this.getMoviesFromStorage();
-    movies.push(newMovie);
-    localStorage.setItem("movies", JSON.stringify(movies));
-}
-
-// get movies from local storage
-Storage.prototype.getMoviesFromStorage = function(){
-    let movies;
-    if(localStorage.getItem("movies") === null){
-        movies = [];
+class Storage{
+    // add new movie to local storage
+    static addMovieToStorage(newMovie){
+        let movies = this.getMoviesFromStorage();
+        movies.push(newMovie);
+        localStorage.setItem("movies", JSON.stringify(movies));
     }
-    else{
-        movies = JSON.parse(localStorage.getItem("movies"));
-    }
-    return movies;
-}
 
-// delete movie from storage
-Storage.prototype.deleteMovieFromStorage = function(title){
-    let movies = this.getMoviesFromStorage();
-    movies.forEach(function(movie, index){ // index: returns index of movie, in array
-        if(movie.title == title){
-            movies.splice(index, 1); // delete one element of array from index
+    // get movies from local storage
+    static getMoviesFromStorage(){
+        let movies;
+        if(localStorage.getItem("movies") === null){
+            movies = [];
         }
-    });
+        else{
+            movies = JSON.parse(localStorage.getItem("movies"));
+        }
+        return movies;
+    }
 
-    localStorage.setItem("movies", JSON.stringify(movies));
-}
+    // delete movie from storage
+    static deleteMovieFromStorage(title){
+        let movies = this.getMoviesFromStorage();
+        movies.forEach(function(movie, index){ // index: returns index of movie, in array
+            if(movie.title == title){
+                movies.splice(index, 1); // delete one element of array from index
+            }
+        });
 
-// clear all movies from storage
-Storage.prototype.clearAllMoviesFromStorage = function(){
-    localStorage.removeItem("movies"); 
+        localStorage.setItem("movies", JSON.stringify(movies));
+    }
+
+    // clear all movies from storage
+    static clearAllMoviesFromStorage(){
+        localStorage.removeItem("movies"); 
+    }
 }
